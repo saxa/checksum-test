6af942d475aa412b3a8ff444cd8055a4284ec3d9
Sasa Ostrouska <casaxa@gmail.com>
2016-05-01 22:56:42 -0300

    Removed checksum-test directory wrongly created and added some code to gchksum.c.

--- src/gchksum.c
+++ src/gchksum.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <glib.h>
 
 #include "gchksum.h"
 
 int main (int argc, char *argv[])
 {
-  if (argc = 0)
+  if (argc = 0){
     usage();
+    exit(0);
+  }
 
   char strptr[100];
+  char chksum[32];
 
   printf("Please enter a string:\n");
   scanf("%s", strptr);
 
-  printf("The entered string is:\n%s", strptr);
+  printf("The entered string is:\n%s\n", strptr);
+  printf("Checksum of the string in md5 is: %s\n", chksum);
 
 return 0;
 }
